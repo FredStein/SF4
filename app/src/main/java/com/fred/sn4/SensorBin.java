d39f6d00eaeb5691d985c7779b8b98903cdfe26a
FredStein <fredstein@internode.on.net>
2017-04-14 20:28:07 +1000

    Create Class skeleton to get(?) and bin sensor data

---
+++ app/src/main/java/com/fred/sn4/SensorBin.java
@@ -0,0 +1,15 @@
+package com.fred.sn4;
+
+/**
+ * Created by Fred Stein on 14/04/2017.
+ */
+
+class SensorBin implements Runnable{
+        long binSize;
+        SensorBin(long binSize) {
+            this.binSize = binSize;
+        }
+        public void run() {
+            // compute primes larger than minPrime
+        }
+}
